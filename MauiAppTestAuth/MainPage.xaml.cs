1d720e7ed44a3647ac3735ac1aa01782467fff9f
Ezequiel Cinalli <ezequielcinalli96@gmail.com>
2023-03-08 09:58:29 -0300

    Added setup for android and windows for WebAuthenticator

--- MauiAppTestAuth/MainPage.xaml.cs
+++ MauiAppTestAuth/MainPage.xaml.cs
@@ -2,23 +2,43 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    private async void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        SemanticScreenReader.Announce(CounterBtn.Text);
+        await Auth();
+    }
+
+    private async Task Auth()
+    {
+        try
+        {
+            WebAuthenticatorResult authResult = await WebAuthenticator.Default.AuthenticateAsync(
+                // new Uri("https://mysite.com/mobileauth/Microsoft"),
+                new Uri("https://www.eternet.com.ar/"),
+                new Uri("myapp://"));
+
+            string accessToken = authResult?.AccessToken;
+
+            // Do something with the token
+        }
+        catch (TaskCanceledException e)
+        {
+            // Use stopped auth
+        }
+    }
 }
 
