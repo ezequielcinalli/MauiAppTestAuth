7c8269df527c12746812e2ce655289ca1c640caf
Ezequiel Cinalli <ezequielcinalli96@gmail.com>
2023-03-07 22:50:10 -0300

    Initial commit with maui project hello world

---
+++ MauiAppTestAuth/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiAppTestAuth;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiAppTestAuth/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiAppTestAuth;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
